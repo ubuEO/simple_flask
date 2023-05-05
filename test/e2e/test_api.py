917e1285e01bae2064c15c2c915392dde87f6a9d
ubuEO <ubueo00@gmail.com>
2023-05-05 07:21:47 +0300

    first_commit

--- test/e2e/test_api.py
+++ test/e2e/test_api.py
@@ -4,12 +4,12 @@ import requests as requests
 from simple_api import config
 
 
-@pytest.mark.usefixtures('restart_api')
+'''@pytest.mark.usefixtures('restart_api')
 def test_api_hello_world():
     url = config.get_api_url()
     r = requests.get(f'{url}')
     assert r.status_code == 200
-    assert r.text == 'Hello, World!'
+    assert r.text == 'Hello, World!'''
 
 
 @pytest.mark.usefixtures('restart_api')
